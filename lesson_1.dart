02bd1eedf640080a253d7c07c3e40bab89549bd1
Hikmatulloh03 <ahmadjanow.03@mail.ru>
2024-06-19 23:09:40 +0600

    Lesson-1

---
+++ lesson_1.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+void main() {
+  String title = 'Курс Dart video';
+  title = 'С нуля до профи дарт (переменные)';
+
+  int likes = 100000;
+
+  // Float == double
+  double rating = 9.4;
+  rating = 7.0;
+
+  bool great = true;
+  great = false;
+
+  // print("Hello from dart!");
+  print(title);
+  print(likes);
+  print(rating);
+  print(great);
+}
